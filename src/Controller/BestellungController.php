8494156e80a621cabe4c2a3a85a7afbb9fe1cdb9
Rainer Albrecht <homealbrecht@gmx.de>
2021-01-15 10:09:47 +0100

    Bestellung

--- src/Controller/BestellungController.php
+++ src/Controller/BestellungController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Bestellung;
+use App\Entity\Gericht;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +18,22 @@ class BestellungController extends AbstractController
         return $this->render('bestellung/index.html.twig', [
             'controller_name' => 'BestellungController',
         ]);
+    }
+
+    /**
+     * @Route("/bestellen/{id}", name="bestellen")
+     */
+    public function bestellen(Gericht $gericht) {
+        $bestellung = new Bestellung();
+        $bestellung->setTisch("tisch1");
+        $bestellung->setName($gericht->getName());
+        $bestellung->setBnummer($gericht->getId());
+        $bestellung->setPreis($gericht->getPreis());
+        $bestellung->setStatus("offen");
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($bestellung);
+        $em->flush();
+        $this->addFlash('bestell', $bestellung->getName(). ' wurde zur Bestellung hinzugefügt');
+        return $this->redirect($this->generateUrl('menu'));
     }
 }
