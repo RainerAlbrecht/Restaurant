f5d6112096e51873e3a775db8d06e46e26a716aa
Rainer Albrecht <homealbrecht@gmx.de>
2021-01-15 06:30:03 +0100

    B-estellung

---
+++ src/Controller/BestellungController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BestellungController extends AbstractController
+{
+    /**
+     * @Route("/bestellung", name="bestellung")
+     */
+    public function index(): Response
+    {
+        return $this->render('bestellung/index.html.twig', [
+            'controller_name' => 'BestellungController',
+        ]);
+    }
+}
